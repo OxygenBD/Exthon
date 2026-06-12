f7f7c776e5961cbbe9c8e42d480dd4cfa49d7afe
OxygenBD <class9backup.one@gmail.com>
2026-06-12 08:38:16 +0600

    feat: Add MainActivity with navigation setup

---
+++ app/src/main/java/com/oxygenbdexthon/activities/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.oxygenbdexthon.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.oxygenbdexthon.R
+import com.oxygenbdexthon.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        setupNavigation()
+        Timber.d("MainActivity created")
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        NavigationUI.setupWithNavController(bottomNav, navController)
+    }
+}
